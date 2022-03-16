4ccb42170c525c5f902c8dcdbf1ede43b445ca10
rsnakdmx <rsnakdmx@hotmail.com>
2022-03-15 22:35:50 -0600

    Se corrigio error para mostrar prerenderizado y se agrego .htacess

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes())
+    ReactDOM.hydrate(<React.StrictMode><App /></React.StrictMode>, rootElement);
+
+else
+    ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, rootElement);
